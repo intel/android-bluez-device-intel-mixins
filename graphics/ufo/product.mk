11c7871a6b929c7a137de1456c63de9b5b1286d9
jlawryno <jacek.lawrynowicz@intel.com>
2013-10-07 14:01:03 -0700

    Added ufo product packages when building from source
    Change-Id: Ia4683b2a885f792d4a92ea5f1edb841e66723cc3

--- graphics/ufo/product.mk
+++ graphics/ufo/product.mk
@@ -3,40 +3,49 @@
 # system/etc
 PRODUCT_PACKAGES += ufo.prop
 
-# system/bin
-PRODUCT_PACKAGES += coreu
-PRODUCT_PACKAGES += mediainfo
+ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true) # ufo packages when prebuilts are used
 
-# system/lib/hw
-PRODUCT_PACKAGES += gralloc.ufo
-PRODUCT_PACKAGES += hwcomposer.default
-PRODUCT_PACKAGES += hwc.ufo
+    # system/bin
+    PRODUCT_PACKAGES += coreu
+    PRODUCT_PACKAGES += mediainfo
 
-# system/lib/egl
-PRODUCT_PACKAGES += libGLES_ufo
-PRODUCT_PACKAGES += libGLES_ufo_intel7
-PRODUCT_PACKAGES += libGLES_ufo_intel7_5
+    # system/lib/hw
+    PRODUCT_PACKAGES += gralloc.ufo
+    PRODUCT_PACKAGES += hwcomposer.default
+    PRODUCT_PACKAGES += hwc.ufo
 
-# system/lib
-PRODUCT_PACKAGES += i965_drv_video
-PRODUCT_PACKAGES += igfxcmjit32
-PRODUCT_PACKAGES += igfxcmrt32
-PRODUCT_PACKAGES += libdrm
-PRODUCT_PACKAGES += libdrm_intel
-PRODUCT_PACKAGES += libgrallocclient
-PRODUCT_PACKAGES += libgrallocgmm
-PRODUCT_PACKAGES += libhwcservice_vpg
-PRODUCT_PACKAGES += libs3cjpeg_vpg
-PRODUCT_PACKAGES += libva
-PRODUCT_PACKAGES += libva-android
-PRODUCT_PACKAGES += libvpwrapper_vpg
-PRODUCT_PACKAGES += lib2d
-PRODUCT_PACKAGES += libcoreuclient
-PRODUCT_PACKAGES += libcoreuinterface
-PRODUCT_PACKAGES += libcoreuservice
-PRODUCT_PACKAGES += libgsmgr
-PRODUCT_PACKAGES += libsubmitdinterface
-PRODUCT_PACKAGES += libuevent
+    # system/lib/egl
+    PRODUCT_PACKAGES += libGLES_ufo
+    PRODUCT_PACKAGES += libGLES_ufo_intel7
+    PRODUCT_PACKAGES += libGLES_ufo_intel7_5
+
+    # system/lib
+    PRODUCT_PACKAGES += i965_drv_video
+    PRODUCT_PACKAGES += igfxcmjit32
+    PRODUCT_PACKAGES += igfxcmrt32
+    PRODUCT_PACKAGES += libdrm
+    PRODUCT_PACKAGES += libdrm_intel
+    PRODUCT_PACKAGES += libgrallocclient
+    PRODUCT_PACKAGES += libgrallocgmm
+    PRODUCT_PACKAGES += libhwcservice_vpg
+    PRODUCT_PACKAGES += libs3cjpeg_vpg
+    PRODUCT_PACKAGES += libva
+    PRODUCT_PACKAGES += libva-android
+    PRODUCT_PACKAGES += libvpwrapper_vpg
+    PRODUCT_PACKAGES += lib2d
+    PRODUCT_PACKAGES += libcoreuclient
+    PRODUCT_PACKAGES += libcoreuinterface
+    PRODUCT_PACKAGES += libcoreuservice
+    PRODUCT_PACKAGES += libgsmgr
+    PRODUCT_PACKAGES += libsubmitdinterface
+    PRODUCT_PACKAGES += libuevent
+
+else # ufo packages when building from source
+
+    PRODUCT_PACKAGES += ufo
+    PRODUCT_PACKAGES += ufo_test
+
+endif
 
 ifneq ($(PANEL_IGNORE_LID),)
     PRODUCT_PROPERTY_OVERRIDES += init.panel_ignore_lid=$(PANEL_IGNORE_LID)
