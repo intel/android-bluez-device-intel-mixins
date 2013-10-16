a3f22a23a63c68924b3de412fc3c3761cfef27cc
Yong Yao <yong.yao@intel.com>
2013-10-16 09:22:48 -0700

    add SFI support for MCG devices
    Change-Id: I16d3ca7ffd0ef405616d111ae6d71e757c061c82
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- boot-arch/sfi/BoardConfig.mk
+++ boot-arch/sfi/BoardConfig.mk
@@ -3,3 +3,29 @@
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_FLASH_BLOCK_SIZE := 512
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+TARGET_MAKE_NO_DEFAULT_BOOTIMAGE ?= true
+
+MKBOOTIMGOSIP := vendor/intel/support/mkbootimg-osip
+
+# Enable to use Intel boot.img
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+TARGET_OS_SIGNING_METHOD := none
+
+MAKE_NO_DEFAULT_BOOTIMAGE_ITEMS = $(MKBOOTIMG) \
+	$(MKBOOTIMGOSIP) \
+	xfstk-stitcher \
+	$(INTERNAL_BOOTIMAGE_FILES) \
+	$(PRODUCT_OUT)/bootstub
+
+# CAUTION: DO NOT CHANGE the flavor of COMMON_BOOTIMAGE_ARGS.  It must remain
+# a recursively-expanded variable, i.e., it must be defined using the '=' sign.
+COMMON_BOOTIMAGE_ARGS = --sign-with $(TARGET_OS_SIGNING_METHOD) \
+	--bootstub $(PRODUCT_OUT)/bootstub
+MAKE_NO_DEFAULT_BOOTIMAGE = $(MKBOOTIMGOSIP) \
+	$(COMMON_BOOTIMAGE_ARGS) \
+	$(INTERNAL_BOOTIMAGE_ARGS) \
+	--product $(TARGET_DEVICE) \
+	--type mos \
+	--output $(INSTALLED_BOOTIMAGE_TARGET) \
+	$(ADDITIONAL_BOOTIMAGE_ARGS)
