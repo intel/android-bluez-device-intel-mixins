c20d83a1f260c21bbb2dedccb277b0fd41705418
Anders Nilsson <anders.nilsson@intel.com>
2013-10-23 15:30:39 +0200

    Add mixin for governor: ondemand and interactive
    Change-Id: Id0cd4652f6e11907a696c4184241d16ba54d03cc
    Signed-off-by: Anders Nilsson <anders.nilsson@intel.com>

---
+++ governor/interactive/BoardConfig.mk
@@ -0,0 +1,2 @@
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/governor/interactive/kernel_defconfig_overlay
+
---
+++ governor/ondemand/BoardConfig.mk
@@ -0,0 +1,2 @@
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/governor/ondemand/kernel_defconfig_overlay
+
