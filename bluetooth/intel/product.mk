b9db1c56f62ea00c868f895a9fc76c0a4a961d64
ola.lilja <ola.lilja@intel.com>
2013-10-24 14:15:19 +0200

    Add mixin for bluetooth
    Change-Id: I6a780ce11b6088b4fa7b5e9cda9593bcfa7f0366
    Signed-off-by: ola.lilja <ola.lilja@intel.com>

---
+++ bluetooth/intel/product.mk
@@ -0,0 +1,17 @@
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_IBT := true
+BOARD_HAVE_BLUETOOTH_BCM := false
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/mixins/bluetooth/intel/buildcfg_include
+BLUETOOTH_HCI_USE_USB := true
+
+# Firmware patch-file
+PRODUCT_COPY_FILES += vendor/intel/hardware/bluetooth/fw/370710018002030d00.seq:system/etc/firmware/370710018002030d00.seq
+
+# Disable kernel BT-drivers
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/bluetooth/intel/kernel_defconfig_overlay
+
+# Bluetooth config
+PRODUCT_PACKAGES += \
+         hciconfig
+
+
