f5a214a92429f8de8f05742a7c48c9e285a33bbb
Piotr Tomaszewski <piotr.tomaszewski@intel.com>
2013-11-06 16:58:28 +0100

    Update storage configuration
    Provide overlay for storage_list.xml that defines
    internal and usb storage.
    
    For: ABRO-262
    
    Change-Id: I97e52dab7d05790eaacb755d165835229072025c

---
+++ storage/no_sdcard/BoardConfig.mk
@@ -0,0 +1,1 @@
+DEVICE_PACKAGE_OVERLAYS += device/intel/mixins/storage/no_sdcard/overlay
--- storage/sdcard/BoardConfig.mk
+++ storage/sdcard/BoardConfig.mk
@@ -1,2 +1,3 @@
 RECOVERY_HAVE_SD_CARD := true
 
+DEVICE_PACKAGE_OVERLAYS += device/intel/mixins/storage/sdcard/overlay
