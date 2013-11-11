dabc4ca8dc229bfe6cd99de92c01a5782c210cf1
Mattias Pettersson <mattias.pettersson@intel.com>
2013-11-11 17:42:38 +0100

    Add sensors mixin for hid_sensorhub.
    Issue: APDEV-49
    
    Change-Id: I83c94ca7a53ffba34375ee894550ddcc49f83f5b
    Signed-off-by: Mattias Pettersson <mattias.pettersson@intel.com>

---
+++ sensors/hid_sensorhub/product.mk
@@ -0,0 +1,7 @@
+# sensor support
+PRODUCT_PACKAGES += \
+        sensors.$(TARGET_PRODUCT)
+
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
