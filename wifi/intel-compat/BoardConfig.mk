e162f01a4600aafc33bbad18f46d3229ac10b726
Oren Givon <oren.givon@intel.com>
2013-11-24 14:20:50 +0200

    mixins: add variables needed by intel-compat wifi
    Set all variables needed in order to build iwlwifi modules
    for an intel board using intel wifi with compat install.
    
    Issue: APDEV-648
    Change-Id: I51985977c802ed91fa21842cc5a7926a5ee55f47
    Depends-Change-Id: I6fc1fe4b184217ccc8aca0db0e0484857999629c
    Depends-Change-Id: I34073ef6746f6579915a86889e0adc4d209998ed
    Depends-Change-Id: I412a7ed02e224d3d13ec98a55ad26298b6b07536
    Depends-Change-Id: Ib813dee745fb0c42f8eb0322a59548d778c35fef
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    Signed-off-by: Hakan Englund <hakan.englund@intel.com>
    Signed-off-by: Mihai Serban <mihai.serban@intel.com>
    Signed-off-by: Hakan Englund <hakan.englund@intel.com>

---
+++ wifi/intel-compat/BoardConfig.mk
@@ -0,0 +1,22 @@
+# For wifi
+include vendor/intel/wifi/wifi.mk
+
+# Set to use WCS supplicant version
+WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL_WCS_COMPAT
+
+#Do not reload firmware when enabling SoftAP
+NO_FW_RELOAD_FOR_SOFTAP := true
+
+BOARD_USING_INTEL_IWL := true
+INTEL_IWL_BOARD_CONFIG := iwlwifi-public-android
+INTEL_IWL_USE_COMPAT_INSTALL := y
+INTEL_IWL_EDIT_MOD_DEP := y
+INTEL_IWL_COMPAT_INSTALL_DIR := updates
+INTEL_IWL_PRODUCT_OUT := $(OUT_DIR)/target/product/$(TARGET_PRODUCT)
+KERNEL_OUT_DIR := $(INTEL_IWL_PRODUCT_OUT)/obj/kernel
+INTEL_IWL_COMPAT_INSTALL_PATH := $(ANDROID_BUILD_TOP)/$(INTEL_IWL_PRODUCT_OUT)/obj/kernelmods
+
+# Special kernel configuration for intel-compat mixin
+# TODO: find a way to use overlays for the default config, this approach is hard
+# to maintain because the files must be kept in sync manually
+TARGET_KERNEL_CONFIG := $(TARGET_KERNEL_ARCH)_bigcore_android_defconfig_wifi_compat
