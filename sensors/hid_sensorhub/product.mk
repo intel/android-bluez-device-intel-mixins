c6b9e78b97e7310fbdd548132465ae0d9454ebf1
Mattias Pettersson <mattias.pettersson@intel.com>
2013-12-09 11:03:58 +0100

    hid_sensorhub: copy XML files for accelerometer and compass
    Also copy XML files for accelerometer and compass into permissions.
    
    Issue: APDEV-49
    
    Change-Id: I25d219a72196af029f5286948533c6b1e3823897
    Signed-off-by: Mattias Pettersson <mattias.pettersson@intel.com>

--- sensors/hid_sensorhub/product.mk
+++ sensors/hid_sensorhub/product.mk
@@ -4,4 +4,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+        frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
