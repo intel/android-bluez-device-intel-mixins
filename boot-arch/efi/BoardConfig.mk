b06f1b1f724897f89dfade3f9b361576804fb960
Andrew Boie <andrew.p.boie@intel.com>
2013-12-09 16:34:48 -0800

    enable sparse EXT images
    Iago has logic to un-sparse the files for its images archive.
    The latest Userfastboot works correctly with sparse images.
    
    Change-Id: I65864ee6cb1179a8f1cb051bf9ca686839d33965
    Depends-Change-Id: Iabe2afda68a8c97d8a40fa4f40505dc224d83b39
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- boot-arch/efi/BoardConfig.mk
+++ boot-arch/efi/BoardConfig.mk
@@ -32,11 +32,9 @@ TARGET_RECOVERY_UI_LIB := libbigcore_recovery_ui
 # EFI System Partition
 TARGET_USERFASTBOOT_LIBS += libufb_esp
 
-# We need to use standard EXT4 images, can't use sparse as the installer
-# needs to loopback mount it. It's in a squashfs container anyway.
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_FLASH_BLOCK_SIZE := 512
-TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
 # Secure boot boot image signing
 TARGET_BOOT_IMAGE_KEY_PAIR ?= build/target/product/security/testkey
