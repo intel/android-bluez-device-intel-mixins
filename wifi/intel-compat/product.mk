d89675d77ef3e86119327d701f8fa4f7dfdc1c5b
benilev <beni.lev@intel.com>
2013-12-16 15:19:31 +0200

    Add P2P support
    Issue: APDEV-648
    Category: Device Enablement
    Domain: AOSP-wifi
    Origin: Internal
    Upstream-Candidate: No
    Change-Id: I1aabcf8b8c3de0a38fc67a53554fe2f14164cb65
    Signed-off-by: benilev <beni.lev@intel.com>

--- wifi/intel-compat/product.mk
+++ wifi/intel-compat/product.mk
@@ -1,9 +1,13 @@
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-        device/intel/mixins/wifi/intel-compat/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+        frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+        device/intel/mixins/wifi/intel-compat/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
 
 # Override WiFi related configs
 TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/wifi/intel-compat/kernel_defconfig_overlay
 
 # include firmware binaries for Wifi adapters
 $(call inherit-product-if-exists, vendor/intel/hardware/PRIVATE/wlan/iwl-firmware/iwlwifi.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        wifi.interface=wlan0
