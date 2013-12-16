4ce83373238d19979a820068bd7fc3dd567d62d1
Muralidhar Dixit <muralidhar.m.dixit@intel.com>
2013-12-16 17:12:04 +0530

    Adding mixin support for miracast
    Adding miracast mixin
    
    Issue: APDEV-991
    Change-Id: I183c1f4d9fca63f2dac70ce369bea5748475b4fe
    Depends-Change-Id: Iae074d28a74488733af8573d51ebf468b5a28ef9
    Signed-off-by: Muralidhar Dixit <muralidhar.m.dixit@intel.com>

---
+++ miracast/intel_miracast/BoardConfig.mk
@@ -0,0 +1,5 @@
+#Enable Mircast support
+INTEL_WIDI := true
+INTEL_WIDI_GEN := true
+INTEL_WIDI_MEDIASDK_ENCODER := true
+
---
+++ miracast/intel_miracast/product.mk
@@ -0,0 +1,12 @@
+#Intel miracast components
+PRODUCT_PACKAGES += \
+	widi.conf \
+	libwidiclient \
+	libwidiservice \
+	libwidimedia \
+	libwidirtsp \
+	libwidiuibc \
+	libwidiuibcjni \
+	WidiInputService \
+	libstragefright_hdcp \
+	libwidirtspsink
