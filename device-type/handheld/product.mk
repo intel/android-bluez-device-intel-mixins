7fa5ff208bc5e7d84e0dbb857d6c01a6a05e95b3
Markus Grape <markus.grape@intel.com>
2013-12-17 12:37:44 +0100

    Add mixins for device-type tablet
    Issue: APDEV-649
    Change-Id: I79bf3d805729f90e7491a8660547ed5467a66fbf
    Signed-off-by: Markus Grape <markus.grape@intel.com>

---
+++ device-type/handheld/product.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
---
+++ device-type/tablet/product.mk
@@ -0,0 +1,5 @@
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
