51dd1938a35af533466cbb808c5614718c37d566
Arik Nemtsov <arik@wizery.com>
2013-12-26 15:11:39 +0200

    Wifi: set BOARD_WLAN_DEVICE to intc
    The hostap build files use the BOARD_WLAN_DEVICE property to enable a
    critical define - ANDROID_P2P. Without this define, the framework cannot
    correctly route commands to wpa_supplicant.
    
    Issue: APDEV-648
    Category: Device Enablement
    Domain: AOSP-wifi
    Origin: Internal
    Upstream-Candidate: No
    Change-Id: I1e925c22edb2acd9bcfe06b1da3854ad8cba1675
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- wifi/intel-upstream/BoardConfig.mk
+++ wifi/intel-upstream/BoardConfig.mk
@@ -3,6 +3,7 @@ include vendor/intel/wifi/wifi.mk
 
 # Set to use WCS supplicant version
 WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL_WCS
+BOARD_WLAN_DEVICE := intc
 
 #Do not reload firmware when enabling SoftAP
 NO_FW_RELOAD_FOR_SOFTAP := true
