d4d776cae1267e9ecdef439e793a88b520bd8aad
Daniel Leung <daniel.leung@intel.com>
2013-12-30 10:57:26 -0800

    graphics: software: force EFI framebuffer to use BGR
    For software renderer, tells gralloc.default to swap red and blue
    channels when using EFI framebuffer. The efifb driver always
    reports RGB.
    
    For: ADEV-310
    Change-Id: I89a71e2001994f33dc67b6fc3df0e1241c354a8c
    Depends-Change-Id: I98735074cde117b02a84e9b8961a303d1da9ebcd
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- graphics/software/BoardConfig.mk
+++ graphics/software/BoardConfig.mk
@@ -2,3 +2,7 @@ BOARD_KERNEL_CMDLINE += vga=current nomodeset
 
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 
+# for gralloc.default
+# Intel Graphics has framebuffer in BGR instead of RGB
+# but efifb still reports RGB. So we need to force it.
+TARGET_GRALLOC_FORCE_EFIFB_PIXEL_FORMAT := "BGRA"
