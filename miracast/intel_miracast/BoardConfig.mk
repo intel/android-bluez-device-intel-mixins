52deee424924f983f7570fb9f276391441b4af49
Muralidhar Dixit <muralidhar.m.dixit@intel.com>
2014-01-15 13:05:24 +0530

    Enable INTEL_WIDI_BAYTRAIL flag for intel_miracast mixin
    Enable INTEL_WIDI_BAYTRAIL in Miracast mixin.Should be reverted
    once HWC changes to INTEL_WIDI_GEN flag
    
    Tracked-By: APDEV-1050
    Change-Id: I41c68ef04d98952630832874bdeeeb842f7c5252
    Depends-Change-Id: Iae074d28a74488733af8573d51ebf468b5a28ef9
    Signed-off-by: Muralidhar Dixit <muralidhar.m.dixit@intel.com>

--- miracast/intel_miracast/BoardConfig.mk
+++ miracast/intel_miracast/BoardConfig.mk
@@ -2,6 +2,7 @@
 INTEL_WIDI := true
 INTEL_WIDI_GEN := true
 INTEL_WIDI_MEDIASDK_ENCODER := true
+INTEL_WIDI_BAYTRAIL := true
 
 #Overlay for enabling WifiDisplay
 DEVICE_PACKAGE_OVERLAYS += device/intel/mixins/miracast/intel_miracast/overlay
