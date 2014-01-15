c287c7ba769163a15f537d6d22e5aba6857cccee
Adriana Reus <adriana.reus@intel.com>
2014-01-15 09:32:40 +0200

    Add selinux mixin
    Add selinux mixin for selinux-related configurations.
    
    Issue: APDEV-913
    Category: Device enablement
    Domain: Security
    Origin: internal
    Upstream-Candidate: No
    Change-Id: Ifba38f9d31cf45db7ae30b909c2cd5c2e0d02ae0
    Signed-off-by: Adriana Reus <adriana.reus@intel.com>

---
+++ selinux/enforcing/product.mk
@@ -0,0 +1,2 @@
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/selinux/enforcing/kernel_defconfig_overlay
+
---
+++ selinux/permissive/product.mk
@@ -0,0 +1,2 @@
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/selinux/permissive/kernel_defconfig_overlay
+
