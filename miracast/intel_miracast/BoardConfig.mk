28cb11cd1351c0a97a4273d453307861714ee602
Muralidhar Dixit <muralidhar.m.dixit@intel.com>
2014-01-17 16:43:41 +0530

    Miracast:Enable Overlay selection for Intel Miracast
    Enable Overlay selection for Intel Miracast mixin. Overlay
    enables WifiDisplay for HSW
    
    Issue: APDEV-991
    Change-Id: I64d702a8bc395b27541ba17d2b2b8d992af66545
    Signed-off-by: Muralidhar Dixit <muralidhar.m.dixit@intel.com>

--- miracast/intel_miracast/BoardConfig.mk
+++ miracast/intel_miracast/BoardConfig.mk
@@ -3,3 +3,5 @@ INTEL_WIDI := true
 INTEL_WIDI_GEN := true
 INTEL_WIDI_MEDIASDK_ENCODER := true
 
+#Overlay for enabling WifiDisplay
+DEVICE_PACKAGE_OVERLAYS += device/intel/mixins/miracast/intel_miracast/overlay
