906bdb0bbeffc824024fbe0b37da4bad0a70c228
Piotr Landowski <piotr.landowski@intel.com>
2014-01-21 13:25:49 +0100

    graphics: Enabling GPU Renderscript driver
    Enabling for baytrail and haswell.
    Other platforms use default CPU RS driver
    
    Change-Id: If61c24afac753597f4c017e65ed191fdc59ee984
    Signed-off-by: Piotr Landowski <piotr.landowski@intel.com>

--- graphics/ufo/BoardConfig.mk
+++ graphics/ufo/BoardConfig.mk
@@ -9,9 +9,24 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 		persist.intel.ogl.debug=/data/ufo.prop \
 		persist.intel.ogl.dumpdebugvars=1 \
 
-# Disable Intel proprietary RS driver, till it's stable
-ADDITIONAL_BUILD_PROPERTIES += \
-		debug.rs.default-CPU-driver=1
+#Intel library for GPU accelerated Renderscript:
+INT1533_PLATFORMS := baytrail haswell
+ifneq ($(filter $(INT1533_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+    OVERRIDE_RS_DRIVER := libRSDriver_intel7.so
+else
+    OVERRIDE_RS_DRIVER := libRSDriver_intel.so
+endif
+
+RSGPU_ENABLED := baytrail haswell
+ifneq ($(filter $(RSGPU_ENABLED),$(TARGET_BOARD_PLATFORM)),)
+    # Enable Intel proprietary RS driver
+    ADDITIONAL_BUILD_PROPERTIES += \
+        debug.rs.default-CPU-driver=0
+else
+    # Disable Intel proprietary RS driver, till it's stable
+    ADDITIONAL_BUILD_PROPERTIES += \
+        debug.rs.default-CPU-driver=1
+endif
 
 ADDITIONAL_DEFAULT_PROPERTIES += ro.ufo.use_coreu=1
 
