54148c4e0b929fae64b1a2121dfdb16905e8ba6c
Markus Grape <markus.grape@intel.com>
2014-01-22 13:35:44 +0100

    device-type: add permission for usb host on tablet
    For: APDEV-650
    Change-Id: Idb6c6602b0edd9d76818ae1965b515e59399800a
    Signed-off-by: Markus Grape <markus.grape@intel.com>

--- device-type/tablet/product.mk
+++ device-type/tablet/product.mk
@@ -1,5 +1,6 @@
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+        frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+        frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
