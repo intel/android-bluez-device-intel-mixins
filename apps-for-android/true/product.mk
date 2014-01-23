51a2fba0549ba267dc42c94a88db1b342615e176
Markus Grape <markus.grape@intel.com>
2014-01-23 13:02:04 +0100

    Add mixins for apps-for-android
    For: APDEV-1232
    Change-Id: Ida3d94be05668a1022bd1022053f96164e0406ba
    Depends-Change-Id: I410f7911c34a077e4dbd3ee15832ff5b3b884491
    Signed-off-by: Markus Grape <markus.grape@intel.com>

---
+++ apps-for-android/true/product.mk
@@ -0,0 +1,22 @@
+# Add CDD applications
+PRODUCT_PACKAGES += \
+        Amazed \
+        AndroidGlobalTime \
+        AnyCut \
+        BTClickLinkCompete \
+        CLiCkin2DaBeaT \
+        DivideAndConquer \
+        Downloader \
+        HeightMapProfiler \
+        LolcatBuilder \
+        Panoramio \
+        Photostream \
+        Radar \
+        RingsExtended \
+        SpriteMethodTest \
+        SpriteText \
+        Translate \
+        Triangle \
+        WebViewDemo \
+        WikiNotes \
+        ReplicaIsland
