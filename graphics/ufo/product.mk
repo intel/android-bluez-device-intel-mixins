88aac596af355f9e25062627fd8d89da20532813
Lukasz Anaczkowski <lukasz.anaczkowski@intel.com>
2014-01-30 14:05:46 +0100

    Use 'ufo_prebuilts' alias module
    Instead of specyfication of all UFO prebuilts
    
    Change-Id: Idd04db820bf2d280e7901d673cdf5ace98ff5718
    Depends-Change-Id: I33e92c58fc5f437d24f2ea4579ed27843fd5c423
    Signed-off-by: Lukasz Anaczkowski <lukasz.anaczkowski@intel.com>
    Reviewed-on: https://android.intel.com/159909
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Wajdeczko, Michal <Michal.Wajdeczko@intel.com>
    Reviewed-by: Kumar-mayernik, Nisha <nisha.kumar-mayernik@intel.com>
    Reviewed-by: Rangasamy, Devaraj <devaraj.rangasamy@intel.com>
    Tested-by: Rangasamy, Devaraj <devaraj.rangasamy@intel.com>
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>

--- graphics/ufo/product.mk
+++ graphics/ufo/product.mk
@@ -5,42 +5,8 @@ PRODUCT_PACKAGES += ufo.prop
 
 ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true) # ufo packages when prebuilts are used
 
-    # system/bin
-    PRODUCT_PACKAGES += coreu
-    PRODUCT_PACKAGES += mediainfo
-    PRODUCT_PACKAGES += curd
-
-    # system/lib/hw
-    PRODUCT_PACKAGES += gralloc.ufo
-    PRODUCT_PACKAGES += hwcomposer.default
-    PRODUCT_PACKAGES += hwc.ufo
-
-    # system/lib/egl
-    PRODUCT_PACKAGES += libGLES_ufo
-    PRODUCT_PACKAGES += libGLES_ufo_intel7
-    PRODUCT_PACKAGES += libGLES_ufo_intel7_5
-
-    # system/lib
-    PRODUCT_PACKAGES += i965_drv_video
-    PRODUCT_PACKAGES += libivp
-    PRODUCT_PACKAGES += igfxcmjit32
-    PRODUCT_PACKAGES += igfxcmrt32
-    PRODUCT_PACKAGES += libdrm
-    PRODUCT_PACKAGES += libdrm_intel
-    PRODUCT_PACKAGES += libgrallocclient
-    PRODUCT_PACKAGES += libgrallocgmm
-    PRODUCT_PACKAGES += libhwcservice_vpg
-    PRODUCT_PACKAGES += libs3cjpeg_vpg
-    PRODUCT_PACKAGES += libva
-    PRODUCT_PACKAGES += libva-android
-    PRODUCT_PACKAGES += libvpwrapper_vpg
-    PRODUCT_PACKAGES += lib2d
-    PRODUCT_PACKAGES += libcoreuclient
-    PRODUCT_PACKAGES += libcoreuinterface
-    PRODUCT_PACKAGES += libcoreuservice
-    PRODUCT_PACKAGES += libgsmgr
-    PRODUCT_PACKAGES += libsubmitdinterface
-    PRODUCT_PACKAGES += libuevent
+    # UFO prebuilts
+    PRODUCT_PACKAGES += ufo_prebuilts
 
 else # ufo packages when building from source
 
