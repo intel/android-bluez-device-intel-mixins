00a61f58731bd2cb9cf5242d07b26c770f137964
Tomas Winkler <tomas.winkler@intel.com>
2014-02-05 14:39:38 +0200

    security: fix comments in product.mk
    The product.mk file was borrowed from another mixin
    and we left an old and unrelated comment there.
    
    Issue: APDEV-1444
    Change-Id: Id429199da5d0287cfec36fdbc7a7b26897a6ab63
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-on: https://android.intel.com/160816
    Reviewed-by: Persson, Mikael <mikael.persson@intel.com>
    Reviewed-by: Usyskin, Alexander <alexander.usyskin@intel.com>
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Englund, Hakan <hakan.englund@intel.com>
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>

--- security/mei/product.mk
+++ security/mei/product.mk
@@ -1,3 +1,3 @@
-# Disable kernel BT-drivers
+# Include MEI/ME Linux kernel drivers and their dependencies
 TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/security/mei/kernel_defconfig_overlay
 
--- security/txei/product.mk
+++ security/txei/product.mk
@@ -1,3 +1,3 @@
-# add mei and txei driver
+# Include MEI/TXE Linux kernel drivers and their dependencies
 TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/security/txei/kernel_defconfig_overlay
 
