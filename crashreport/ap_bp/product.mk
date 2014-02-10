a1dca4438352275d823fdf2d3c453bd30855a42e
Anisha <anisha.dattatraya.kulkarni@intel.com>
2014-02-10 11:27:20 -0800

    Crashreport : Set CRASH_REPORT_PARTIAL_REPORT to true.
    Add variable CRASH_REPORT_PARTIAL_REPORT to product.mk files.
    This variable is set to true which is used as a switch to disable
    FULL_REPORT. Indirectly it enables logcat dumps in crash reports.
    
    Issue: APDEV-1321
    Change-Id: I1190874adb753b818c8bd72803b647212d3d227c
    Depends-Change-Id: I0566edfd4a650ef6f5189ec5fa21c168fca2a8c5
    Depends-Change-Id: Ib1dd2f27f1c74648f8f0dc90441debf16e16032e
    Signed-off-by: Anisha Dattatraya Kulkarni <anisha.dattatraya.kulkarni@intel.com>
    Reviewed-on: https://android.intel.com/162201
    Reviewed-by: Leung, Daniel <daniel.leung@intel.com>

--- crashreport/ap_bp/product.mk
+++ crashreport/ap_bp/product.mk
@@ -1,5 +1,10 @@
 # Crash Report (AP and BP)
 
+# CRASH_REPORT_PARTIAL_REPORT is a switch used to disable FULL_REPORT.
+# This change is used as a way to enable logcat dumps
+# in crash report.
+CRASH_REPORT_PARTIAL_REPORT := true
+
 # Enable modem crash report
 CRASH_REPORT_MODEM_SUPPORT := true
 
--- crashreport/ap_only/product.mk
+++ crashreport/ap_only/product.mk
@@ -1,5 +1,10 @@
 # Crash Report (AP only)
 
+# CRASH_REPORT_PARTIAL_REPORT is a switch used to disable FULL_REPORT.
+# This change is used as a way to enable logcat dumps
+# in crash report
+CRASH_REPORT_PARTIAL_REPORT := true
+
 # Disable modem crash report
 CRASH_REPORT_MODEM_SUPPORT :=
 
