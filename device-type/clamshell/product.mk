9f779bb45098d23d96662d84fab4af175ae1dea0
Axel Fagerstedt <axel.fagerstedt@intel.com>
2014-02-10 19:58:07 +0100

    device-type: Do not disable soft keyboard
    EventHub detects the built in PS/2 controller as a physical keyboard
    and disables the soft keyboard. The input service has been patched
    to ignore the specific device, but the "feature" needs to be enabled
    by setting ro.ignore_atkbd to not zero.
    
    Enabling the feature for tablet and phone devices.
    
    As we do not want to enable this for laptop style designs (hsb) a new
    device type, clamshell is introduced and a corresponding patch to update
    the hsb confifuration has been created.
    
    Issue: APDEV-1361
    
    Depends-Change-Id: Ie8e22ef0d3053492faad10040399d6bc1ec9c62b
    
    Change-Id: Ic6e11bf6df735f4403c3e5f6626d3e738926c721
    Signed-off-by: Axel Fagerstedt <axel.fagerstedt@intel.com>
    Reviewed-on: https://android.intel.com/162191
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>

--- device-type/clamshell/product.mk
+++ device-type/clamshell/product.mk
@@ -3,5 +3,3 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
         frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
-
-PRODUCT_PROPERTY_OVERRIDES += ro.ignore_atkbd=1
--- device-type/handheld/product.mk
+++ device-type/handheld/product.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_PROPERTY_OVERRIDES += ro.ignore_atkbd=1
