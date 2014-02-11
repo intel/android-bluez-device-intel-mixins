95ba749ed4a6fb9a30f0f86366ba57c29e9dd002
Robert Chiras <robert.chiras@intel.com>
2014-02-11 16:29:08 +0200

    Revert "Baytrail: Update of widevine for Baytrail"
    This reverts commit a2dc0ce8f3874df95afa5c788838bd56a281b61b.
    
    Change-Id: I29f41714eaff2ecd30021de1e094732673e69d15
    Reviewed-on: https://android.intel.com/162597
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Tested-by: Chiras, Robert <robert.chiras@intel.com>

--- widevine/classic/product.mk
+++
@@ -1,2 +0,0 @@
-$(call inherit-product-if-exists, vendor/google/PRIVATE/widevinedrm/widevine.mk)
-$(call inherit-product-if-exists, vendor/intel/PRIVATE/DRM/widevine/$(TARGET_BOARD_PLATFORM)/widevine.mk)
