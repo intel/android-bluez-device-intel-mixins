0d494cf18232605cbbed0dad03ce723b9ded1921
Andrew Boie <andrew.p.boie@intel.com>
2014-02-11 10:50:43 -0800

    bluetooth: move directives to proper place
    The build needs to complete even if vendor/ is missing, so migrate
    the PRODUCT_COPY_FILES directives that include the firmware files
    to another Makefile which is invoked using inherit-product-if-exists.
    
    Move some directives out of product.mk which don't belong there;
    only PRODUCT_* variables should be changed in a product.mk; the
    rest belongs in BoardConfig.mk
    
    Issue: ADEV-449
    Change-Id: Ib39defcba521e769fceb822f0e37943c713275ea
    Depends-Change-Id: Icb920263d4cf6dc4e0e1ea5f87df1829604637c8
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/162716
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>

--- bluetooth/intel/product.mk
+++ bluetooth/intel/product.mk
@@ -1,23 +1,10 @@
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_IBT := true
-BOARD_HAVE_BLUETOOTH_BCM := false
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/mixins/bluetooth/intel/buildcfg_include
-BLUETOOTH_HCI_USE_USB := true
-
-# Firmware patch-file
 PRODUCT_COPY_FILES += \
-        vendor/intel/hardware/bluetooth/fw/370710010002030d36.seq:system/etc/firmware/370710010002030d00.seq \
-        vendor/intel/hardware/bluetooth/fw/370710010002030d36.seq:system/etc/firmware/370710010002030d36.seq \
-	vendor/intel/hardware/bluetooth/fw/370710018002030d36.seq:system/etc/firmware/370710018002030d00.seq \
-	vendor/intel/hardware/bluetooth/fw/370710018002030d36.seq:system/etc/firmware/370710018002030d36.seq \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
         frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
-
-# Disable kernel BT-drivers
-TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/bluetooth/intel/kernel_defconfig_overlay
 
 # Bluetooth config
 PRODUCT_PACKAGES += \
          hciconfig
 
+$(call inherit-product-if-exists,vendor/intel/hardware/bluetooth/fw/btfw.mk)
 
