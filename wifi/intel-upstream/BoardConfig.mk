b7210350f491263007e2e7a8fbaf55f7aae3304c
Andrew Boie <andrew.p.boie@intel.com>
2014-02-11 10:25:53 -0800

    wifi: update location and move Makefile directives here
    The wifi.c has been moved under hardware/intel/wifi as it is
    needed at build time to create the wifi HAL; the build must
    not fail if the vendor/ tree is missing.
    
    Some Makefile logic that was present in vendor/intel/wifi
    has instead been moved directly into the mixin.
    
    Issue: ADEV-449
    Change-Id: If6398a7afe0027b12adb59dac196ff76d7b6df33
    Depends-Change-Id: Ib69ea26fabef3b2ef534727291d273dcb7cece94
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/162709
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Leung, Daniel <daniel.leung@intel.com>

--- wifi/intel-upstream/BoardConfig.mk
+++ wifi/intel-upstream/BoardConfig.mk
@@ -1,5 +1,11 @@
 # For wifi
-include vendor/intel/wifi/wifi.mk
+BOARD_CUSTOM_WIFI_HAL_SRC := hardware/intel/wifi/wifi.c
+ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0
+
+# Must be set to enable WPA supplicant build. WEXT is the driver for generic
+# linux wireless extensions. NL80211 supercedes it.
+# This variable is used by external/wpa_supplicant/Android.mk
+BOARD_WPA_SUPPLICANT_DRIVER=NL80211
 
 # Set to use WCS supplicant version
 WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL_WCS
