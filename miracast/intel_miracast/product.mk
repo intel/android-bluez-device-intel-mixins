0bd399eb54f74a7826b8c1689a3390dc96945dfa
Nikhil Bhandary <nikhil.bhandary@intel.com>
2014-02-13 00:33:01 +0530

    Adding Miracast Compliance components PRODUCT_PACKAGES
    Adding Miracast Compliance related components to
    PRODUCT_PACKAGES so that they can be the part
    of default build. This should be build for eng
    and userdebug builds only.
    
    Issue: APDEV-1077
    Depends-Change-Id: I429117b011cdbc1f680454ccabc71d699ee2c810
    Change-Id: Ibb349e2bfa1d38ed0fd49997168a2d9cbfd85f44
    Signed-off-by: Nikhil Bhandary <nikhil.bhandary@intel.com>
    Reviewed-on: https://android.intel.com/162718
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Breana, Tiberiu A <tiberiu.a.breana@intel.com>
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

--- miracast/intel_miracast/product.mk
+++ miracast/intel_miracast/product.mk
@@ -11,7 +11,31 @@ PRODUCT_PACKAGES += \
 	libstagefright_hdcp \
 	libwidirtspsink
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
 #Intel miracast test components
 PRODUCT_PACKAGES += \
 	widiMultitaskingPlayer
 
+#Intel Miracast Compliance components
+PRODUCT_PACKAGES += \
+	libsigmacapi \
+	com.intel.widi.sigmaapi \
+	libwidisigmajni \
+	com.intel.widi.sigmaapi.xml \
+	WirelessDisplaySigmaCapiUI \
+	testsigmartspcmds
+
+#Intel Miracast Compliance Sigma Agent
+PRODUCT_PACKAGES += \
+	wifi_wfa \
+	libwfa \
+	wfa_ca \
+	wfa_dut \
+	wfa_service \
+	wfa_send_ping \
+	wfa_send_ping6 \
+	wfa_stop_ping \
+	cas.pem \
+	root.pem \
+	wifiuser.pem
+endif
