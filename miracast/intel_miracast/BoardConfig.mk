1c48f0914b6b6238758c3e1de6814f399ba6dca4
Nikhil Bhandary <nikhil.bhandary@intel.com>
2014-02-12 12:52:12 +0530

    Revert "Enable INTEL_WIDI_BAYTRAIL flag for intel_miracast mixin"
    This reverts commit 52deee424924f983f7570fb9f276391441b4af49.
    No more required as INTEL_WIDI_GEN is enabled
    
    Change-Id: Ib0f209a7963df65593380ea4a0957652ce70665f
    Reviewed-on: https://android.intel.com/162309
    Reviewed-by: Bhandary, Nikhil <nikhil.bhandary@intel.com>
    Tested-by: Bhandary, Nikhil <nikhil.bhandary@intel.com>
    Reviewed-by: Aghara, Sanjay R <sanjay.r.aghara@intel.com>
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Tested-by: Aghara, Sanjay R <sanjay.r.aghara@intel.com>
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>

--- miracast/intel_miracast/BoardConfig.mk
+++ miracast/intel_miracast/BoardConfig.mk
@@ -2,7 +2,6 @@
 INTEL_WIDI := true
 INTEL_WIDI_GEN := true
 INTEL_WIDI_MEDIASDK_ENCODER := true
-INTEL_WIDI_BAYTRAIL := true
 
 #Overlay for enabling WifiDisplay
 DEVICE_PACKAGE_OVERLAYS += device/intel/mixins/miracast/intel_miracast/overlay
