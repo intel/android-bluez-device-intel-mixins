69485a79e0cf0a79bd71692f0845b7f51a412f48
Bogdan George Stefan <bogdan.g.stefan@intel.com>
2014-02-14 16:21:20 +0200

    Add alx668 audio mixin for basic audio playback
    Adding the alc668 configuration file alongside the
    alc282 configuration file is enough to have the dell
    play sounds. There is no need for a separate folder
    structure or a new build target. Since the alc282
    and alc668 are identical, I am copying the same file
    with both names
    
    Change-Id: Ic04ec90c23493070765f6761119f388546dd909b
    Signed-off-by: Bogdan George Stefan <bogdan.g.stefan@intel.com>
    Category: Audio
    Domain: Devices
    Origin: internal
    Upstream-Candidate: No, proprietary
    Reviewed-on: https://android.intel.com/163800
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Reviewed-by: Breana, Tiberiu A <tiberiu.a.breana@intel.com>
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>
    Reviewed-by: Timohi, Alexandru <alexandru.timohi@intel.com>
    Reviewed-by: Suman, Viorel <viorel.suman@intel.com>
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

--- audio/hsw_alc282/product.mk
+++ audio/hsw_alc282/product.mk
@@ -5,7 +5,8 @@ PRODUCT_COPY_FILES += \
 	$(hsw_alc282_path)/audio_policy.conf:system/vendor/etc/audio_policy.conf \
 	$(hsw_alc282_path)/mixer_paths_Analog_Devices.xml:system/etc/mixer_paths_Analog_Devices.xml \
 	$(hsw_alc282_path)/mixer_paths_unknown.xml:system/etc/mixer_paths_unknown.xml \
-	$(hsw_alc282_path)/mixer_paths_ALC282.xml:system/etc/mixer_paths_ALC282.xml
+	$(hsw_alc282_path)/mixer_paths_ALC282.xml:system/etc/mixer_paths_ALC282.xml \
+	$(hsw_alc282_path)/mixer_paths_ALC282.xml:system/etc/mixer_paths_ALC668.xml
 
 # Audio low latency support
 PRODUCT_COPY_FILES += \
