d4a935b479b0261fd1f0f9d38cc6e6b3eb800bb7
johan.bohlin <johan.bohlin@intel.com>
2014-02-18 14:27:38 +0100

    audio: hsw_alc282: added support to use the mcg hal
    Added support to use the mcg hal for haswell with alc282.
    For: APDEV-1078
    Depends-Change-Id: I26c4006215ad3a5af8178b5d53a71b44773b9be0
    Depends-Change-Id: I2b2ea5b43b3948425cf0b24272d6ad45786c7af5
    
    Change-Id: I1a8ea6be928fab2bd0a1dc4851af57a6877f121d
    Signed-off-by: johan.bohlin <johan.bohlin@intel.com>
    Reviewed-on: https://android.intel.com/164674
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>

---
+++ audio/hsw_alc282_mcghal/product.mk
@@ -0,0 +1,82 @@
+#
+# Audio HAL
+#
+TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/audio/hsw_alc282_mcghal/kernel_defconfig_overlay
+
+# Hardware HAL
+PRODUCT_PACKAGES += \
+    audio_hal_configurable \
+    libaudioresample \
+    audio.a2dp.default \
+    vibrator.$(PRODUCT_NAME) \
+    audio.usb.$(PRODUCT_NAME)
+
+# Tinyalsa tools
+PRODUCT_PACKAGES += \
+    tinyplay \
+    tinycap \
+    tinymix \
+    tinypcminfo
+
+# Parameter-framework files
+PRODUCT_PACKAGES += \
+    parameter-framework.audio.bayleybay \
+    libparameter \
+    parameter-connector-test \
+    libxmlserializer \
+    liblpe-subsystem \
+    libtinyamixer-subsystem \
+    libtinyalsactl-subsystem \
+    libfs-subsystem \
+    libproperty-subsystem \
+    parameter
+
+# HDMI audio HAL
+PRODUCT_PACKAGES += \
+   audio.hdmi.$(PRODUCT_NAME)
+
+
+# NXP audio effects
+PRODUCT_PACKAGES += \
+    libbundlewrapper.so \
+    libreverbwrapper.so \
+    libxmlparser.so \
+    LvmDefaultControlParams.xml \
+    LvmSessionConfigurationMinus1.xml
+
+# Remote submix audio
+PRODUCT_PACKAGES += \
+       audio.r_submix.default
+
+# Specific management of audio_effects.conf
+PRODUCT_COPY_FILES += \
+    device/intel/mixins/audio/hsw_alc282_mcghal/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+# Audio policy file
+PRODUCT_COPY_FILES += \
+    device/intel/mixins/audio/hsw_alc282_mcghal/audio_policy.conf:system/etc/audio_policy.conf
+
+# Audio asound file
+PRODUCT_COPY_FILES += \
+    device/intel/mixins/audio/hsw_alc282_mcghal/asound.conf:system/etc/asound.conf
+
+# Remote-process for parameter-framework tuning interface
+ifneq (, $(findstring "$(TARGET_BUILD_VARIANT)", "eng" "userdebug"))
+PRODUCT_PACKAGES += \
+    libremote-processor \
+    remote-process
+endif
+
+# SpeechRecorder for eng build variant
+ifneq ($(filter $(TARGET_BUILD_VARIANT),eng),)
+PRODUCT_PACKAGES += \
+    SpeechRecorder
+endif
+
+# AudioToolBox (for eng builds)
+#ifneq (, $(findstring "$(TARGET_BUILD_VARIANT)", "eng"))
+# PRODUCT_PACKAGES += \
+#     AudioToolBox
+#endif
+
+
