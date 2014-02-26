325f0aea8dfec5302cb183c432b7f0fb2354eef7
Alexandru Timohi <alexandru.timohi@intel.com>
2014-02-26 10:42:45 +0200

    mkbooting with a siging key and a passphrase
    Add a passphrase arg to getsignature.sh. The passphrase can be from STDIN
    
    Change-Id: I3a956d2e304aafae6ea842a9b326c785f842c147
    Issue: APDEV-1438
    Signed-off-by: Alexandru Timohi <alexandru.timohi@intel.com>
    Reviewed-on: https://android.intel.com/166877
    Tested-by: sys_abtbuild <sys_abtbuild@intel.com>
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Reviewed-by: Petrescu, Rares <rares.petrescu@intel.com>
    Reviewed-by: Breana, Tiberiu A <tiberiu.a.breana@intel.com>
    Reviewed-by: Suman, Viorel <viorel.suman@intel.com>
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- boot-arch/efi/getsignature.sh
+++ boot-arch/efi/getsignature.sh
@@ -7,7 +7,12 @@ function alldone {
     exit $1
 }
 
-openssl pkcs8 -inform DER -nocrypt -in $1 -out $KF || alldone 1
+if [ $# -gt 1 ]; then
+    openssl pkcs8 -inform DER -in $1 -passin pass:$2 -out $KF || alldone 1
+else
+    openssl pkcs8 -inform DER -nocrypt -in $1 -out $KF || alldone 1
+fi
+
 openssl dgst -sha256 -sign $KF || alldone 2
 alldone 0
 
