690cde143664d5e0713f3fd77077396556e08007
Alexandru Timohi <alexandru.timohi@intel.com>
2014-04-11 12:22:55 +0300

    Add audio mixin for ALC271, needed by ivybridge platform
    Signed-off-by: Robert Chiras <robert.chiras@intel.com>

---
+++ audio/ivb_alc271/BoardConfig.mk
@@ -0,0 +1,7 @@
+AUDIO_HAL := audio_pc
+DEVICE_PACKAGE_OVERLAYS += $(hsw_alc282_path)/overlay
+
+#MDP
+USE_INTEL_MDP := false
+#IPP
+USE_INTEL_IPP ?= false
---
+++ audio/ivb_alc271/product.mk
@@ -0,0 +1,30 @@
+hsw_alc282_path := device/intel/mixins/audio/hsw_alc282
+
+# Audio codec support
+PRODUCT_COPY_FILES += \
+	$(hsw_alc282_path)/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+	$(hsw_alc282_path)/mixer_paths_Analog_Devices.xml:system/etc/mixer_paths_Analog_Devices.xml \
+	$(hsw_alc282_path)/mixer_paths_unknown.xml:system/etc/mixer_paths_unknown.xml \
+	$(hsw_alc282_path)/mixer_paths_ALC282.xml:system/etc/mixer_paths_ALC271X.xml \
+	$(hsw_alc282_path)/mixer_paths_ALC282.xml:system/etc/mixer_paths_ALC668.xml
+
+# Audio low latency support
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+
+# Tinyalsa
+PRODUCT_PACKAGES += \
+	tinymix \
+	tinyplay \
+	tinycap
+
+# Audio support
+PRODUCT_PACKAGES += \
+	audio.primary.$(TARGET_PRODUCT) \
+	audio.a2dp.default \
+	audio.hsp.default
+
+# Remote submix device required for miracast
+PRODUCT_PACKAGES += \
+	audio.r_submix.default
+
