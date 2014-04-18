704d7c881b56b61900e7fad19d235ebb6264b66e
Tiberiu Breana <tiberiu.a.breana@intel.com>
2014-04-18 12:35:22 +0300

    Add Minnowboard device type
    Added a new device-type mixin for the MinnowBoard MAX, which
    provides its own 'tablet_core_hardware.xml', to better reflect
    the device's capabilities.
    
    Issue: APDEV-2873
    Issue: APDEV-2901
    
    Change-Id: I8069add3e007382d753c57dcee44af49a2328f11
    Depends-Change-Id: Idad85a70a77bc9fb851d2519866c09f40c7187cb
    Signed-off-by: Tiberiu Breana <tiberiu.a.breana@intel.com>
    Reviewed-on: https://android.intel.com/182380
    Reviewed-by: Petrescu, Rares <rares.petrescu@intel.com>
    Tested-by: Vlaescu, CristianX <cristianx.vlaescu@intel.com>
    Reviewed-by: Musca, Constantin <constantin.musca@intel.com>
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

---
+++ device-type/minnowboard/product.mk
@@ -0,0 +1,9 @@
+PRODUCT_CHARACTERISTICS := tablet
+
+ADDITIONAL_DEFAULT_PROPERTIES += fw.max_users=10
+
+PRODUCT_COPY_FILES += \
+        device/intel/mixins/device-type/minnowboard/minnowboard_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+        frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+PRODUCT_PROPERTY_OVERRIDES += ro.ignore_atkbd=1
