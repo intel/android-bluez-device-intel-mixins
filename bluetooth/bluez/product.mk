6bcc69e56e28b7adf7db4cf3c6ac070e3eeebaad
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2014-05-07 13:51:11 +0300

    android-bluez: Source bluetooth firmware makefile
    Change-Id: I57fdafd1624ee2d310070420a1836c59bb119669

--- bluetooth/bluez/product.mk
+++ bluetooth/bluez/product.mk
@@ -1,3 +1,5 @@
 BOARD_HAVE_BLUETOOTH := true
 
 TARGET_KERNEL_CONFIG_OVERRIDES += device/intel/mixins/bluetooth/bluez/kernel_defconfig_overlay
+
+$(call inherit-product-if-exists, vendor/intel/hardware/bluetooth/fw/intel.mk)
